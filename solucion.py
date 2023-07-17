0d4455de94d0dafa8a811c396d5fce54bec0d181
nubol23 <rafael.rvp98@gmail.com>
2023-07-16 23:00:16 -0400

    Setup pd-lab-3

---
+++ solucion.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def ej_1_cargar_csv() -> pd.DataFrame:
+    pass
+
+
+def ej_2_puntuacion_promedio(df: pd.DataFrame) -> float:
+    pass
+
+
+def ej_3_nombre_estudiantes(df: pd.DataFrame) -> list[str]:
+    pass
+
+
+def ej_4_edad_y_cantidad(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def ej_5_edad_promedio(df: pd.DataFrame) -> float:
+    pass
