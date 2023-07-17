8eb62a988a6247a6d84ba3359f0eff640ef9fa61
nubol23 <rafael.rvp98@gmail.com>
2023-07-17 10:56:33 -0400

    Setup lab 4

---
+++ solucion.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def ej_1_cargar_iso_codes() -> list[str]:
+    pass
+
+
+def ej_2_cargar_tipos_de_cambio() -> None:
+    pass
+
+
+def ej_3_convertir(moneda_origen: str, monto: float, moneda_destino: str) -> float:
+    pass
