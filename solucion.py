2aad9cea9feddba0f7397feca6165c86921ca16e
nubol23 <rafael.rvp98@gmail.com>
2023-07-16 21:36:33 -0400

    Setup pd-lab-2 branch

---
+++ solucion.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def ej_1_cargar_csv() -> pd.DataFrame:
+    pass
+
+
+def ej_2_convertir_fecha(df: pd.DataFrame):
+    pass
+
+
+def ej_3_convertir_cantidad(df: pd.DataFrame):
+    pass
+
+
+def ej_4_fecha_como_indice(df: pd.DataFrame):
+    pass
+
+
+def ej_5_filtrar_por_fecha(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def ej_6_filtrar_por_producto(df: pd.DataFrame) -> pd.DataFrame:
+    pass
