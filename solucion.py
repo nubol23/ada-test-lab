d4de67b55089c44d576d4b8014ce3f37c729709b
nubol23 <rafael.rvp98@gmail.com>
2023-07-17 11:37:18 -0400

    Finis lab setup

--- solucion.py
+++ solucion.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import requests
 
 
 def ej_1_cargar_iso_codes() -> list[str]:
     pass
 
 
-def ej_2_cargar_tipos_de_cambio() -> None:
+def ej_2_cargar_tipos_de_cambio(monedas: list[str]) -> None:
+    # Nota: pueden existir monedas que no tengan correspondencia en la API
+    # asegurate que el código de la respuesta es ok (200)
     pass
 
 
