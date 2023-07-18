fc2d3ec6e30b98fd68619cbece9ec4fc4207dfad
nubol23 <rafael.rvp98@gmail.com>
2023-07-17 23:45:33 -0400

    Setup test pd 5

---
+++ solucion.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def ej_1_leer_archivo(direccion_archivo: str) -> list[str]:
+    pass
+
+
+def ej_2_lista_diccionarios(lineas: list[str]) -> list[dict[str, str]]:
+    pass
+
+
+def ej_3_convertir_a_dataframe(datos: list[dict[str, str]]) -> pd.DataFrame:
+    pass
+
+
+def ej_4_limpieza(df: pd.DataFrame) -> None:
+    pass
