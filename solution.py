b488b0d67e0d835ae2fa9a651b9d5b425748cfa8
Gabriela <gabimartinez883@gamil.com>
2023-08-12 21:45:24 -0600

    test_lab_3

--- solution.py
+++ solution.py
@@ -1,2 +1,10 @@
-def validate_date(date: str) -> str:
-    pass
+def validate_date(date: str):
+   valoraño = int(input("6 7 531:"))
+   d, m, a =list(map(int, date.split()))
+   if valoraño % 4 ==0 and valoraño % 100!=0:
+      print(valoraño, "es un año bisiesto")
+   else:
+      print(valoraño , "no es un año bisiesto")
+      return valoraño
+   validate_date()
+
